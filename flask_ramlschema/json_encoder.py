9f33540662ccffcd3c067db2313f7075589bf6b3
colton <lwcolton@gmail.com>
2016-07-10 14:37:20 -0500

    add encoding for datetime object

--- flask_ramlschema/json_encoder.py
+++ flask_ramlschema/json_encoder.py
@@ -1,8 +1,12 @@
+import datetime
 import json
+
 from bson import ObjectId
 
 class JSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
+    def default(self, object_value):
+        if isinstance(object_value, ObjectId):
+            return str(object_value)
+        elif isinstance(object_value, datetime.datetime):
+            return int(object_value.timestamp())
+        return json.JSONEncoder.default(self, object_value)
